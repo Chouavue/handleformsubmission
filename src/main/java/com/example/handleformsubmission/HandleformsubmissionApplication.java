3fa811e46c4cc50cc0731dee33b7e06a4b54c020
Choua Vue <vuec23@gmail.com>
2022-06-03 10:02:26 -0400

    first commit

---
+++ src/main/java/com/example/handleformsubmission/HandleformsubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.handleformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleformsubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleformsubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/handleformsubmission/HandleformsubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handleformsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleformsubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
